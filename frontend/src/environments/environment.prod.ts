444604fb8753a6ab23aea8351f36e5a588489559
Ignacio Smail <nachitosmail@gmail.com>
2025-11-30 23:34:24 -0300

    fix render backend api

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,6 +2,5 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://whjzdqgiwabydgvsmxex.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoanpkcWdpd2FieWRndnNteGV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5NjQ2MzMsImV4cCI6MjA3ODU0MDYzM30.H88dJVe87gkRMthUx2r_7Cx7hSq1S93b_zko7lQY0xw',
-  api: 'https://uni-materias-backend.onrender.com/api'
-
+  api: 'https://uni-materias.onrender.com/api',  // 👈 CAMBIADO
 };
